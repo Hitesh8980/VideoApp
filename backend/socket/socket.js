a6cdb3245c5627729b35eaca3619cb458aacc408
unknown <hitesh.sharma176@gmail.com>
2025-02-14 17:09:52 +0530

    made changes in backend

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -1,10 +1,9 @@
-
 const { Server } = require("socket.io");
 
 function setupSocket(server) {
   const io = new Server(server, {
     cors: {
-      origin: "*",
+      origin: "*", 
       methods: ["GET", "POST"]
     }
   });
@@ -12,20 +11,40 @@ function setupSocket(server) {
   io.on("connection", (socket) => {
     console.log("A user connected: ", socket.id);
 
+    // Send the socket ID to frontend
+    socket.emit("me", socket.id);
+
+    // Handle user joining a room
     socket.on("join-room", (roomId, userId) => {
       socket.join(roomId);
       socket.to(roomId).emit("user-connected", userId);
     });
 
+    // Handle user signaling
     socket.on("signal", (data) => {
       io.to(data.room).emit("signal", data);
     });
 
+    // Handle call initiation
+    socket.on("callUser", (data) => {
+      io.to(data.userToCall).emit("callUser", { 
+        signal: data.signalData, 
+        from: data.from, 
+        name: data.name 
+      });
+    });
+
+    // Handle answering a call
+    socket.on("answerCall", (data) => {
+      io.to(data.to).emit("callAccepted", data.signal);
+    });
+
+    // Handle call disconnection
     socket.on("disconnect", () => {
+      socket.broadcast.emit("callEnded");
       console.log("User disconnected: ", socket.id);
     });
   });
 }
 
 module.exports = { setupSocket };
-
